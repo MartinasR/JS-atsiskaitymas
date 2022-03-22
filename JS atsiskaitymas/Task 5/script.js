27c1936c81fb2dab2ba56c6f2e002359575a0663
MartinasR <martinasrudis@gmail.com>
2022-03-22 10:48:23 +0200

    uzduotis 5

--- JS atsiskaitymas/Task 5/script.js
+++ JS atsiskaitymas/Task 5/script.js
@@ -19,3 +19,13 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+let filterDogOwners = (masyvas) => {
+  return masyvas.filter(item => item.hasDog)
+}
+console.log(filterDogOwners(users))
+
+let filterAdults = (masyvas) => {
+  return masyvas.filter(item => item.age >= 18)
+}
+console.log(filterAdults(users))
